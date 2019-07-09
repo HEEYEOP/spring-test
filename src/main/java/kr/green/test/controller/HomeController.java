ed43696a2ffbbaa95ba8107a687df71b3152456f
gmlduq <gmlduqrhdwn@naver.com>
2019-07-09 12:23:52 +0900

    DB연동 및 메소드

--- src/main/java/kr/green/test/controller/HomeController.java
+++ src/main/java/kr/green/test/controller/HomeController.java
@@ -1,4 +1,4 @@
-package kr.green.test;
+package kr.green.test.controller;
 
 import java.text.DateFormat;
 import java.util.Date;
@@ -6,34 +6,29 @@ import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-/**
- * Handles requests for the application home page.
- */
+import kr.green.test.dao.MemberDAO;
+
 @Controller
 public class HomeController {
 	
+	@Autowired
+	MemberDAO memberDao;
+	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
+	public String home(Model model) {
+		logger.info("Welcome home! The client locale is {}.");
 		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		System.out.println(memberDao.getEmail("12345678"));
 		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
 		
 		return "home";
 	}
-	
 }
---
+++ src/main/java/kr/green/test/service/MemberServiceImp.java
@@ -0,0 +1,5 @@
+package kr.green.test.service;
+
+public class MemberServiceImp {
+
+}
